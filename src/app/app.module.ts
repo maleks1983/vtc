bb37314a88c6de3dff2583f0e730b4af76464799
MAleksandr <maleks1983@gmail.com>
2023-08-15 13:14:42 +0300

    create services block

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -65,7 +65,7 @@ import { MainBlackoutComponent } from './component/main-blackout/main-blackout.c
       },
       {
         path: '**',
-        redirectTo: ""
+        redirectTo:''
       },
     ])
 
